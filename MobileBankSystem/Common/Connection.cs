f078fe215f5429a8f07a7fd49298bfdf6539c543
Ognjen Gatalo <ognjeng95@gmail.com>
2017-11-16 09:44:23 +0100

    Dodao Server i Connection klasu

---
+++ MobileBankSystem/Common/Connection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    [ServiceContract]
+    public class Connection : IConnection
+    {
+        [OperationContract]
+        public void TestConnection(string testNeki)
+        {
+            Console.WriteLine(testNeki);
+        }
+    }
+}
