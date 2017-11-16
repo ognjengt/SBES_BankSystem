233743b64d44b89a1b00b8a9b8de12b10b54857e
sofijailic <sofijailic505@gmail.com>
2017-11-16 16:30:28 +0100

    Napravila klasu User
    u Commonu klasa User

---
+++ MobileBankSystem/Common/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public SecureString Password { get; set; }
+        public string Uloga { get; set; }
+        public string BrojRacuna { get; set; }
+
+
+        public User(string username, SecureString password,string uloga,string brRacuna ) {
+
+            this.Username = username;
+            this.Password = password;
+            this.Uloga = uloga;
+            this.BrojRacuna = brRacuna;
+        }
+    }
+
+   
+}
